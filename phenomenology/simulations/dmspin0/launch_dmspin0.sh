4c277ab0bc79a4798d8fd02a007b3439158b347c
CMS user <cms-user0@colav.co>
2022-04-25 13:09:13 -0500

    dmsimp0 simulations

---
+++ phenomenology/simulations/dmspin0/launch_dmspin0.sh
@@ -0,0 +1,25 @@
+# launch the simulations for a signal with a scalar mediator 
+launch
+	shower=Pythia
+	detector=Delphes
+	
+	set nevents 50000
+	set MY0 100
+	set MXd 10
+
+launch
+	shower=Pythia
+        detector=Delphes
+        
+        set nevents 50000
+        set MY0 1000
+        set MXd 100
+        
+launch
+	shower=Pythia
+        detector=Delphes
+        
+        set nevents 50000
+        set MY0 5000
+        set MXd 1000
+        
