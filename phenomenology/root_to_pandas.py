186f57d1cb3a3a2aba39622a6b54750fc6b75516
deoache <deoache@udea.edu.co>
2022-05-03 10:34:49 -0500

    process dmspin0 samples

--- phenomenology/root_to_pandas.py
+++ phenomenology/root_to_pandas.py
@@ -1,36 +1,39 @@
+import json
+import argparse
 import pandas as pd
 from utils import get_dataframe
 
-datasets = {
-    "dmspin0": {
-        "my100_mx1000": [
-            "/cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/Events/run_01/tag_2_delphes_events.root",
-            "/cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/Events/run_02/tag_2_delphes_events.root",
-            "/cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/Events/run_03/tag_2_delphes_events.root",
-        ],
-        "my100_mx100": [
-            "/cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/Events/run_04/tag_2_delphes_events.root",
-            "/cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/Events/run_05/tag_1_delphes_events.root",
-            "/cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/Events/run_06/tag_1_delphes_events.root",
-        ],
-        "my5000_mx1000": [
-            "/cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/Events/run_07/tag_1_delphes_events.root",
-            "/cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/Events/run_08/tag_1_delphes_events.root",
-            "/cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/Events/run_09/tag_1_delphes_events.root",
-        ],
-    }
-}
 
-variables = {
-    "MissingET": ["Phi", "MET"],
-    "Jet": ["PT", "Eta", "Phi", "Mass"],
-}
+def main(args):
+    # samples to be converted
+    with open(
+        "/home/cms-user0/VBFDM_UdeA/phenomenology/dmspin0_samples.json", "r"
+    ) as f:
+        dmspin0_samples = json.load(f)
 
-output_df = pd.DataFrame()
-for dataset, fnames in datasets["dmspin0"].items():
-    for fname in fnames:
-        output_df = pd.concat(
-            [output_df, get_dataframe(fname, variables, label=dataset)], axis=0
-        )
+    # variables to read
+    variables = {"MissingET": ["Phi", "MET"], "Jet": ["PT", "Eta", "Phi", "Mass"]}
 
-output_df.to_csv("dmspin0.csv")
+    output_df = pd.DataFrame()
+    for dataset, fnames in dmspin0_samples.items():
+        for fname in fnames:
+            output_df = pd.concat(
+                [output_df, get_dataframe(fname, variables, label=dataset)], axis=0
+            )
+
+    output_df.to_csv(f"{args.fpath}/dmspin0.csv")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--fpath",
+        dest="fpath",
+        default="/.",
+        type=str,
+        help="folder to save the csv file",
+    )
+    args = parser.parse_args()
+
+    main(args)
