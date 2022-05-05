43437ed4311a8f7c24dcb23ec9a2cac8f41bbf48
deoache <deoache@udea.edu.co>
2022-05-04 19:02:36 -0500

    update spin0 simulation

--- phenomenology/simulations/dmspin0/run_simulation.sh
+++ phenomenology/simulations/dmspin0/run_simulation.sh
@@ -1,11 +1,11 @@
 for i in 1 2 4 5 6 7 8 9 10 11 12
 do
-    python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/bin/madevent launch_dmspin0_mx10_my100.sh
+        python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/bin/madevent launch_dmspin0_mx10_my100.sh
 done
 
 for i in 1 2 3 4 5 6
 do
-	python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/bin/madevent launch_dmspin0_mx100_my1000.sh
+        python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/bin/madevent launch_dmspin0_mx100_my1000.sh
 done
 
 for i in 1 2 3
