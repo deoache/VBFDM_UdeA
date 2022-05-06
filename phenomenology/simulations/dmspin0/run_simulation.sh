dc53f6461f6d666f7d2cc2215dfcd961c478b21f
deoache <deoache@udea.edu.co>
2022-05-06 09:31:51 -0500

    update simulation run

--- phenomenology/simulations/dmspin0/run_simulation.sh
+++ phenomenology/simulations/dmspin0/run_simulation.sh
@@ -1,14 +1,14 @@
-for i in 1 2 4 5 6 7 8 9 10 11 12
+for i in $(seq 1 $1) 
 do
         python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/bin/madevent launch_dmspin0_mx10_my100.sh
 done
 
-for i in 1 2 3 4 5 6
+for i in $(seq 1 $2) 
 do
         python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/bin/madevent launch_dmspin0_mx100_my1000.sh
 done
 
-for i in 1 2 3
+for i in $(seq 1 $3) 
 do
 	python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin0_VBF/bin/madevent launch_dmspin0_mx1000_my5000.sh
 done
--- phenomenology/simulations/dmspin1/run_simulation.sh
+++ phenomenology/simulations/dmspin1/run_simulation.sh
@@ -1,14 +1,12 @@
-for i in 1 2 4 5 6 7 8 9 10 11 12
-do
+for i in $(seq 1 $1) 
+do 
 	python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin1_VBF/bin/madevent launch_dmspin1_mx10_my100.sh
 done
-
-for i in 1 2 3 4 5 6
+for i in $(seq 1 $2)
 do
 	python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin1_VBF/bin/madevent launch_dmspin1_mx100_my1000.sh
 done
-
-for i in 1 2 3
+for i in $(seq 1 $3)
 do
 	python2 /cms/mc/MG5_aMC_v3_1_1/DMsimp_spin1_VBF/bin/madevent launch_dmspin1_mx1000_my5000.sh
 done
